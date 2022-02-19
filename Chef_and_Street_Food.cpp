8670e080f54a426b7e9ced228552fa7539121ea6
Yuvraj Singh <yuvraj0807.be20@chitkara.edu.in>
2022-02-19 21:52:03 +0530

    going with the flow

---
+++ Chef_and_Street_Food.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        
+        int s,p,v;
+        cin >> s>>p>>v;
+        if(p/s != 0){
+            cout << ((p/s)+p%s)*v<<endl;
+        }else{
+            cout <<"0"<<endl;
+        }
+    }
+}
