e43922016e93ff220166e7b2ad17cb125442293e
Yuvraj Singh <yuvraj0807.be20@chitkara.edu.in>
2022-02-19 19:48:45 +0530

    Warming up

---
+++ Add_Two_Numbers.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b;
+        cin >> a>>b;
+        if(a,b>=0){
+            int sum=a+b;
+            cout << sum <<endl;
+
+        }
+    }
+    return 0;
+}
