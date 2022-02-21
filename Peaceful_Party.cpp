deb6273ca28b6bbfe7efb9f123535bb2432c3a1e
Yuvraj Singh <yuvraj0807.be20@chitkara.edu.in>
2022-02-21 21:09:52 +0530

    Feb Cook Off

---
+++ Peaceful_Party.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+c>b)
+        {
+            cout<<a+c<<endl;
+        }
+        else
+        {
+            cout<<b<<endl;
+        }        
+    }
+}
